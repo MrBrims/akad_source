a88748068e10a634e1697cd2f86f7148864f81e4
Acryt <acryt@mail.com>
2024-03-19 16:17:21 +0300

    form-blitz, tt: cap, margin admin menu

---
+++ source/parts/sections/blitz.php
@@ -0,0 +1,10 @@
+<section class="section blitz">
+	<div class="container">
+		<h2 class="blitz__title">Seien Sie unter den Ersten, die über Rabatte und Sonderangebote informiert werden!</h2>
+		<p class="blitz__sub">Hinterlassen Sie Ihre E-Mail und erhalten Sie Informationen über attraktive Angebote! Nur günstige Angebote
+			ohne Spam und unnötige Informationen.</p>
+		<?php get_template_part('parts/blocks/form-blitz'); ?>
+		<p class="blitz__text">Die Hinweise aus der <a href="<?php echo get_bloginfo('url'); ?>/datenschutz/">Datenschutzerklärung</a> und den <a href="<?php echo get_bloginfo('url'); ?>/agb/">AGB</a> habe ich gelesen und akzeptiere diese</p>
+		<p class="blitz__text">Ihre Daten werden streng vertraulich behandelt und nicht an Dritte weitergegeben</p>
+	</div>
+</section>
