2eaeb176bbaaf13b0d05cd86a3b4277e7e26dbdf
MrBrims <magistrbrims@gmail.com>
2024-05-14 13:13:07 +0300

    fix shema

--- source/parts/shema/microdata-single.php
+++ source/parts/shema/microdata-single.php
@@ -1,19 +1,19 @@
 <?php
 $current_url = home_url() . $_SERVER['REQUEST_URI'];
-$random_rating = rand(49, 50) / 10;
-$random_all_rating = rand(372, 391);
+$random_rating = rand(48, 50) / 10;
+$random_all_rating = rand(372, 745);
 ?>
 
 <script type="application/ld+json">
 	{
 		"@context": "https://schema.org",
 		"@type": "CreativeWorkSeries",
-		"name": "<?php the_title(); ?>",
+		"name": "Akademily",
 		"aggregateRating": {
 			"@type": "AggregateRating",
 			"bestRating": "5",
 			"ratingValue": "<?php echo $random_rating; ?>",
 			"ratingCount": "<?php echo $random_all_rating; ?>"
-		},
+		}
 	}
 </script>
